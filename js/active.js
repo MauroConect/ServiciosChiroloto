825b44e6ff4036219359624e8ae9dcd7a40eb447
MauroConect <maurot970@gmail.com>
2022-11-29 13:24:50 -0300

    cambios en texto, seccion contacto, boton wpp

--- js/active.js
+++ js/active.js
@@ -81,13 +81,6 @@
         });
     }
 
-    // :: 5.0 ScrollUp Active Code
-    if ($.fn.scrollUp) {
-        browserWindow.scrollUp({
-            scrollSpeed: 1500,
-            scrollText: '<i class="fa fa-angle-up"></i>'
-        });
-    }
 
     // :: 6.0 CouterUp Active Code
     if ($.fn.counterUp) {
