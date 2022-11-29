d3d01bec0503c99915e32654ee57e8aa1a878d69
MauroConect <maurot970@gmail.com>
2022-11-29 13:40:05 -0300

    tamaño boton contacto

--- js/google-map/map-active.js
+++ js/google-map/map-active.js
@@ -19,7 +19,7 @@ var mapOptions = {
 };
 map = new google.maps.Map(document.getElementById("googleMap"), mapOptions);
 var geocoder_map = new google.maps.Geocoder();
-var address = 'New york';
+var address = 'Bahia Blanca';
 geocoder_map.geocode({
     'address': address
 }, function (results, status) {
